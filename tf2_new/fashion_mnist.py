166a4a2d983c3f5bac7ac9ed084758e8d49a3c4c
imjwpak83 <imjwpak83@naver.com>
2019-10-05 10:20:54 +0900

    20191005 10:20

--- tf2_new/fashion_mnist.py
+++ tf2_new/fashion_mnist.py
@@ -1,9 +1,26 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
 class FashionModel:
     def __init__(self):
         pass
 
     def execute(self):
-        pass
+        fashion_mnist = keras.datasets.fashion_mnist
+        (train_images, train_labels),(test_images, test_labels) = fashion_mnist.load_data()
+        class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                       'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+        print('-------Train set spec ----------')
+        print('훈련이미지 :', train_images.shape)
+        print('훈련이미지 수 :', len(train_labels))
+        print('훈련이미지 라벨:', train_labels)
+        print('-------Test set spec ----------')
+        print('테스트이미지 :', test_images.shape)
+        print('테스트이미지 수:', len(test_labels))
+        print('테스트이미지 라벨:', test_labels)
 
 if __name__ == '__main__':
     f = FashionModel()
